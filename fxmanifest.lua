c3ab5afba4bad43db339352b731807fefa99391b
nsilvan1 <55840140+nsilvan1@users.noreply.github.com>
2022-04-14 09:27:58 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+ 
+files {
+    'data/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+
+shared_script "@ThnAC/natives.lua"
+client_script "@ThnAC/stopper.lua"
